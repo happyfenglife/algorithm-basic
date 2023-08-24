d362f634ff387b4f7c8f5c86dfcddeac94c0d3ee
happyfenglife <happyfenglife@qq.com>
2023-08-24 09:27:18 +0800

    Nearest left

---
+++ src/class01/Code05_BSNearLeft.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+ public:
+  // 在arr上，找满足>=value的最左位置
+  static int nearestIndex(const vector<int> &v, int k) {
+    int L = 0;
+    int R = v.size() - 1;
+    int t = -1; //  记录最左的对号
+
+    while (L <= R) {
+      int M = L + ((R - L) >> 1);
+
+      if (v[M] >= k) {
+        t = M;
+        R = M - 1;
+      } else {
+        L = M + 1;
+      }
+    }
+
+    return t;
+  }
+};
+
+int main() {
+  int testTime = 500000;
+  int maxSize = 100;
+  int maxValue = 100;
+  bool succeed = true;
+  // 创建随机数引擎
+  random_device rd;      // 用于获取种子
+  mt19937 gen(rd()); // Mersenne Twister引擎，以rd()作为种子
+
+  // 创建均匀分布
+  uniform_int_distribution<> sdist(1, maxSize);
+  uniform_int_distribution<> vdist(1, maxValue);
+
+  for (int i = 0; i < testTime; ++i) {
+    vector<int> v(sdist(gen));
+    for (auto &j : v)
+      j = vdist(gen);
+
+    sort(v.begin(), v.end());
+
+    int k = vdist(gen);
+
+    int j = Solution::nearestIndex(v, k);
+    int t = distance(v.begin(), lower_bound(v.begin(), v.end(), k));
+    t = (t == v.size() ? -1 : t);
+
+    if (j != t) {
+      cout << j << endl;
+      cout << t << endl;
+      succeed = false;
+      break;
+    }
+  }
+
+  cout << (succeed ? "Nice" : "Fucking fucked") << endl;
+  return 0;
+}
